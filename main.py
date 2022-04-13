bd08cbf44e26c15c900568618780767e5fdf4c00
daniel.gunn@palm.com <4oESUv0U>
2022-04-13 14:06:26 +0800

    Adding PySimpleGUI integration to input settings from user

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from win32com.client import constants
 from win32com.client.gencache import EnsureDispatch as Dispatch
 from datetime import datetime
 from bs4 import BeautifulSoup
+import PySimpleGUI as psg
+
+# input from the user the settings
+subject_filter = psg.popup_get_text("Enter the subject search string",default_text=" online class on ")
+start_date = psg.popup_get_date(1,1,title="First day",no_titlebar=False)
+start_date = '{}-{}-{} 00:01 AM'.format(start_date[2],start_date[0],start_date[1])
+FILE_TYPES_CSV = (("CSV Files", "*.csv *.csv"),)
+output_file = psg.popup_get_file("Save as",default_extension="csv",default_path="rows_per_day.csv",file_types=FILE_TYPES_CSV)
 
 # Thank you for advice on using the win32com API from:
 # https://www.codeforests.com/2021/05/16/python-reading-email-from-outlook-2/?msclkid=059450b5bacf11ec9d31f1c4756891a2
@@ -24,34 +32,25 @@ class Oli():
         ''' helping to introspect outlook object exposing available properties (methods and attributes) '''
         return sorted( self._obj._prop_map_get_.keys() )
 
-data = {}
+rows_per_day = {}
 
 for inx, folder in Oli(mapi.Folders).items():
-    # iterate all Outlook folders (top level)
-    print("-"*70)
-    print(folder.Name)
-
     for inx,subfolder in Oli(folder.Folders).items():
         if subfolder.Name == "Inbox":
-            print("(%i)" % inx, subfolder.Name,"=> ", subfolder)
-            today = datetime.now()
-
-            # first day of the month
-            start_time = today.replace(month=1, hour=0, minute=0, second=0).strftime('%Y-%m-%d %H:%M %p')
             messages = subfolder.Items
-            messages = messages.Restrict("[ReceivedTime] >= '" + start_time + "'")
-            messages = messages.Restrict("@SQL=(urn:schemas:httpmail:subject LIKE '%online class on%')")
+            messages = messages.Restrict("[ReceivedTime] >= '" + start_date + "'")
+            messages = messages.Restrict("@SQL=(urn:schemas:httpmail:subject LIKE '%{}%')".format(subject_filter))
             for message in list(messages)[:30]:
                 #print(message.Subject, message.ReceivedTime, message.SenderEmailAddress)
                 soup = BeautifulSoup(message.HTMLBody, "html.parser")
-                student_count = len(soup.find_all('tr'))
+                row_count = len(soup.find_all('tr'))
                 day = message.ReceivedTime.strftime("%Y-%m-%d")
-                if day in data:
-                    data[day] = max(data[day], student_count)
+                if day in rows_per_day:
+                    rows_per_day[day] = max(rows_per_day[day], row_count)
                 else:
-                    data[day] = student_count
+                    rows_per_day[day] = row_count
 
-with open('output.csv', 'w') as f:
+with open(output_file, 'w') as f:
     f.write('Date,Student Absences\n')
-    for key in data.keys():
-        f.write("%s, %s\n" % (key, data[key]))
+    for key in rows_per_day.keys():
+        f.write("%s, %s\n" % (key, rows_per_day[key]))
